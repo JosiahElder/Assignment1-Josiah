16d058aa0b29803e020d7c871fae75bb62308eeb
JosiahElder <@JosiahElder>
2023-02-01 09:25:41 -0800

    Next Version

--- Main.java
+++ Main.java
@@ -1,13 +1,28 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);
+        Scanner inp = new Scanner(System.in);
 
         System.out.println("FANTASY HOCKEY APPLICATION\n" +
                 "TEAM ENTRY\n" +
                 "================================");
 
+        //--------------------------------------------------
+        int no_of_teams = 3;
+        Team[] T = new Team[no_of_teams];
+        String tName;
 
+        for (int i=0;i<no_of_teams;i++)
+        {
+            System.out.println("Enter name for team #" + (i+1));
+            tName = inp.nextLine();
+            T[i] = new Team(tName);
+        }
+
+        for (int i=0;i<no_of_teams;i++)
+        {
+            System.out.println(T[i].getName());
+        }
 
     }
 }
