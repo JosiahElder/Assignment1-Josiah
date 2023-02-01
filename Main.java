a4a872d60117a2161cee0a4215ee07941e2c218d
JosiahElder <123614077+JosiahElder@users.noreply.github.com>
2023-01-31 18:55:25 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.println("FANTASY HOCKEY APPLICATION\n" +
+                "TEAM ENTRY\n" +
+                "================================");
+
+    }
+}
